7ccacd975ebdeb0c09d0c2749707bc88bf6d9d54
kamble0805 <sekamblehome@gmail.com>
2025-05-04 19:03:47 +0530

    sqlite 3

--- coaching_system/settings.py
+++ coaching_system/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'coaching_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coaching_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
